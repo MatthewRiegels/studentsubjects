8c2d381c85566e1810596f693d6d116eed9f1a6c
matthewriegels <riegels.mj@oundleschool.org.uk>
2023-10-18 12:00:44 +0100

    iufksf

--- addsubjects.php
+++ addsubjects.php
@@ -1,5 +1,5 @@
 <?php
-header('Location: users.php'); // if it breaks, comment this line first
+// header('Location: users.php'); // if it breaks, comment this line first
 
 include_once("connection.php");
 
@@ -8,7 +8,7 @@ array_map("htmlspecialchars", $_POST);
 echo $_POST["subjectname"]."<br>";
 echo $_POST["teachername"]."<br>";
 
-$stmt = $conn->prepare("INSERT INTO TblUsers (SubjectID,Subjectname,Teacher)VALUES (null,:subjectname,:teachername)");
+$stmt = $conn->prepare("INSERT INTO TblSubjects (SubjectID,Subjectname,Teacher)VALUES (null,:subjectname,:teachername)");
 $stmt->bindParam(':subjectname', $_POST["subjectname"]);
 $stmt->bindParam(':teachername', $_POST["teachername"]);
 $stmt->execute();
