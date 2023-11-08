24f4e7a67bf3ad50d4f08adc8c443909cdc2fa7e
matthewriegels <riegels.mj@oundleschool.org.uk>
2023-11-08 10:31:10 +0000

    nofeiYFNOHBIVYTC

--- addtoclass.php
+++ addtoclass.php
@@ -1,5 +1,5 @@
 <?php
-header('Location: pupildoessubject.php'); // if it breaks, comment this line first
+header('Refresh:2; url= pupildoessubject.php'); // if it breaks, comment this line first
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -13,6 +13,18 @@ try{
     $stmt->bindParam(':subjectid', $_POST["subject"]);
     $stmt->bindParam(':userid', $_POST["student"]);
     $stmt->execute();
+
+    $stmt = $conn->prepare("SELECT * FROM TblUsers WHERE UserID = :userid");
+    $stmt->bindParam(':userid', $_POST["student"]);
+    $stmt->execute();
+    $stud_arr = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    $stmt = $conn->prepare("SELECT * FROM TblSubjects WHERE SubjectID = :subjectid");
+    $stmt->bindParam(':subjectid', $_POST["subject"]);
+    $stmt->execute();
+    $subj_arr = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    echo($stud_arr["Surname"] . ', ' . $stud_arr["Forename"] . ' -> ' . $subj_arr["Subjectname"] . ' (' . $subj_arr["Teacher"] . ')');
 }
 catch (PDOException $e){
     echo "error".$e->getMessage();
